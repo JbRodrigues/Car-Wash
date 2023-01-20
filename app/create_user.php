298563079e856296001638219df7835a927db31b
JbRodrigues <juliobisaia@hotmail.com>
2023-01-20 15:05:57 -0300

    First Commit

---
+++ app/create_user.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create User - Barber Shop</title>
+</head>
+
+<body>
+    <main>
+        <form action="get">
+            <label for="create_login">User Login</label>
+            <input type="text" id="create_login">
+            <label for="create_name">Username</label>
+            <input type="text" id="create_name">
+            <label for="create_email">Email</label>
+            <input type="email" id="create_email">
+            <label for="create_phone">Phone number</label>
+            <input type="tel" id="create_phone" >
+            <input type="button" value="send" id="create_send">
+            <input type="reset" value="clear" id="create_clear">
+        </form>
+    </main>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BarberShop</title>
+</head>
+
+<body>
+    <main>
+        <div class="index_icon">
+            <img src="" alt="barber icon">
+        </div>
+
+        <form action="get">
+            <input type="text" name="index_login">
+            <input type="password" name="index_password" id="index_password">
+            <input type="submit" value="Login" class="index_button" id="index_send">
+            <input type="reset" value="Clear" class="index_button" id="index_reset">
+        </form>
+        <a type="submit" href="app/create_user.php">Sign in</a>
+
+    </main>
+
+    <footer>
+        <h5>Created by: JbRodrigues</h5>
+    </footer>
+</body>
+
+</html>
