ce263b2bb52f6ba5380111f38d134220e19f8a5c
JbRodrigues <juliobisaia@hotmail.com>
2023-01-20 15:55:10 -0300

    Change the project ideia to a Car Wash booking system

--- app/index.php
+++ app/index.php
@@ -10,7 +10,7 @@ $create_name = "Julio Bisaia"
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel - Barber Shop</title>
+    <title>Panel - Car Wash</title>
 </head>
 
 <body>
